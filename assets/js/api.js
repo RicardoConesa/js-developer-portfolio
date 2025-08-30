8f6a3e6e7e8c0b4ef7e2f1995b68863c9671bc41
Ricardo Conesa <ricardo.conesa@gmail.com>
2025-08-30 16:12:54 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,6 @@
-
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/RicardoConesa/js-developer-portfolio/refs/heads/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+    const url = 'https://raw.githubusercontent.com/RicardoConesa/js-developer-portfolio/main/data/profile.json';
+    const response = await fetch(url);
+    const profileData = await response.json();
+    return profileData;
 }
